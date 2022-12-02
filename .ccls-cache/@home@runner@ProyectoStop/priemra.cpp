20ce657cfc53e46250eae487455f7a64a60d7270
natalia castro <116594722+Nat209@users.noreply.github.com>
2022-12-02 02:58:33 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@ProyectoStop/priemra.cpp
@@ -0,0 +1,125 @@
+#include <ctime>
+#include <iostream>
+#include <string>
+using namespace std;
+
+string findSubstr(string str, int n) {
+  if (str.length() < n) {
+    return str;
+  }
+
+  return str.substr(0, n);
+}
+
+string LetraAleatoria() { // esto es para lanzar una letra aleatoria
+  string aleatoria[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i",
+                        "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                        "s", "t", "u", "v", "w", "x", "y", "z"};
+  string letra = aleatoria[rand() % 26];
+  return (letra);
+}
+
+string espaciar( int valor){
+  int espacio =0;
+  string texto ="";
+  espacio = tamano;
+    for (int i=0; i<espacio;i++){
+    texto = texto + " ";
+    }
+  return texto;
+  }
+
+int main() {
+  string str[7];
+  string Jugador, letra;
+  int partidas;
+  string pre1, pre2;
+  int puntaje = 0;
+  
+
+  cout << "\nSumercé bienvenido al juego stop!\nDebe escribir  'si' o "
+          "'no'\n¿Desea iniciar el juego? ";
+  cin >> pre1;
+
+  if (pre1 == "si") {
+
+    cout << "\nTe explicamos como:" << endl;
+
+    cout << "\nAl comienzo te pediremos tu nombre en ese momento aun no "
+            "comenzara el juego, ya escrito tu nombre y al darle enter "
+            "comenzara el contador y ahi si comienzra la partida, despues de "
+            "eso el juego te arrojara una letra del abecedario al azar\n ¿En "
+            "que consiste el juego?\n El juego te ira haciendo unas serie de "
+            "preguntas como por ejemplo escribe un nombre de un pais y tendras "
+            "que escribir el pais, pero con la regla de que ese nombre de ese "
+            "pais se escriba con la letra que se te dio ya antes menciondo "
+            "como por ejemplo: se te dio la letra c el nombre del pais seria "
+            "colombia o canadá.\n";
+
+    cout << "\nIngrese nombre del jugador : "; // esto es para ingresar datos
+                                               // del jugador.
+    cin >> Jugador;
+    cout << "Cuantas partidas desea jugar: ";
+    cin >> partidas;
+
+    for (int i = 1; i <= partidas; i++) {
+      
+     letra = LetraAleatoria();
+
+      cout << "La letra a jugar es: " << letra << endl << endl;
+
+      cout << "Ronda Número: " << i << endl << endl;
+
+      srand(time(NULL));
+      cout << "1) ingrese un nombre: ";
+      cin >> str[0];
+      cout << "2) ingrese un apellido: ";
+      cin >> str[1];
+      cout << "3) ingrese un animal: ";
+      cin >> str[2];
+      cout << "4) ingrese una fruta: ";
+      cin >> str[3];
+      cout << "5) ingrese un pais: ";
+      cin >> str[4];
+      cout << "6) ingrese un color: ";
+      cin >> str[5];
+      cout << "7) ingrese una cosa: ";
+      cin >> str[6];
+
+      int n = 1;
+      
+      for (int j = 0; j <= 6; j++) {
+        string substr = findSubstr(str[j], n);
+        cout << substr << ' ';
+        if(substr == letra){
+          puntaje = puntaje +100;
+        }if(substr == ""){
+          puntaje = puntaje +0;
+        }if(substr != letra){
+          puntaje = puntaje -100;
+        }
+      }
+      cout << puntaje;
+    }
+    cout << Jugador << " este es el resultado del juego: " << endl;
+
+    cout << "------------------------------------------------------------------"
+            "------------------------ \n";
+    cout << "letra     Nombre     Apellido       Animal       Fruta        País     Color    Cosa    Puntaje  \n";
+    cout << "------------------------------------------------------------------"
+            "-------------------------\n";
+     cout << letra<<espaciar(5);
+    for (int i =0; i<=6;i++){
+    cout <<   espaciar( 5) << espaciar(5);
+      }
+    cout <<espaciar(5)<< puntaje;
+    
+    cout << endl<< "------------------------------------------------------------------\n";
+   
+
+  } else {
+    cout << "\nGracias por visitarnos sumercé!" << endl;
+  }
+
+  return 0;
+}
